c4898498e1ae2e72c9777f1be1eb04b576a3c1fe
mojidev-py <pycharmdudeig@gmail.com>
2024-09-02 15:16:56 -0700

    made foundations for intents and channel

--- discmoji/channel.py
+++ discmoji/channel.py
@@ -3,5 +3,10 @@ import aiohttp
 
 
 
-class Channel:
-    """Represents the base channel object, all other channel objects inherit from."""
+class GuildTextChannel:
+    def __init__(self,_data: dict):
+        self.id = _data["id"]
+        self.type = 0       
+        self.position = _data["position"]
+        # will be an object once Intents are made
+        self.overwrites = _data["permission_overwrites"] 
