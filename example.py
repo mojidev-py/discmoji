a7c5696b5a721779e1c03f4092861b1660db4e42
mojidev-py <pycharmdudeig@gmail.com>
2024-09-02 17:34:08 -0700

    got decos working fine, such a relief

--- example.py
+++ example.py
@@ -7,11 +7,12 @@ Client = Bot(token="blah blah",intents=123213123)
 # this is subject to change
 
 
-@Client.command(name="test1 one")
+@Client.command(name="test1")
 def commd(ctx: Invoked):
     asyncio.run(Client.get_guild(1234567))
 
-@commd.error
+@commd.error()
+# debug: Unwrap _Wrapped on
 def error_cool(ctx: Invoked,error: Exception):
     print("ooh thank you demo-py!!")
 
