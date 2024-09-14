9c6de879ed9b3114b490f4db4c20a92ddb3e172a
mojidev-py <pycharmdudeig@gmail.com>
2024-09-14 16:31:19 -0700

    100th commit and also changed up overwrites.py

--- discmoji/overwrites.py
+++ discmoji/overwrites.py
@@ -6,7 +6,10 @@ import asyncio
 class ChannelPermissionOverwrites:
     
     def __init__(self,_data: dict,__bindedbot: Bot,__bindedguild: Guild):
-        self.role = asyncio.run(__bindedbot._http.send_request('get',f'/guilds/{__bindedguild.id}/roles/{_data["permission_overwrites"] if len(_data["permission_overwrites"]) > 1 else None}'))
-        # else None will be handled in another attribute, (roles)
+        try:
+            self.role = asyncio.run(__bindedbot._http.send_request('get',f'/guilds/{__bindedguild.id}/roles/{_data["permission_overwrites"]["id"] if len(_data["permission_overwrites"]) > 1 else None}'))
+        except Exception as e:
+            self.roles = ...
         self.allowed_perms = 0
         self.disabled_perms = 0 
+        
