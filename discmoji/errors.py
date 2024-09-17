aac24bc76a4f0fe3b8ea99c28a5aef3a7967a44f
XenonPydev <134782258+XenonPy@users.noreply.github.com>
2024-09-16 18:05:01 -0700

    Add more specific error classes

--- discmoji/errors.py
+++ discmoji/errors.py
@@ -1,13 +1,22 @@
-
 class DiscmojiError(Exception):
     """ An error relating to the inner workings of Discmoji. """
     def __init__(self,msg: str):
         self.msg = msg
     def __str__(self):
-        return "Discmoji encountered an internal error."
+        return f"Discmoji encountered an internal error: {self.msg}"
+
+class DiscmojiAPIError(DiscmojiError):
+    """ An error related to Discord API interactions in Discmoji. """
+    def __str__(self):
+        return f"An error occured when interacting with the Discord API: {self.msg}"
+class DiscmojiCommandError(DiscmojiError):
+    """ An error related to bot commands using Discmoji. """
+    def __str__(self):
+        return f"An error ocurred when executing/creating a bot command: {self.msg}"
    
-   # format to call: DiscmojiError('error text')
-   # how this is called: DiscmojiError("blueblahhhh")
-   # output: 
+   # For unknown/unspecified errors: `raise DiscmojiError("Error Message")`
+   # For Discord API errors: `raise DiscmojiAPIError("Error Message")`
+   # For command errors: `raise DiscmojiCommandError("Error Message")`
+   # -- sample output --
    # DiscmojiError: Discmoji encountered an internal error while trying to initiate a connection or another error occured that raised this exception. error with message:
    # (Exception,(the default/builtin exception that it failed with's args)) blueblahhh
