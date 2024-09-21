2f9ec60db4bcc7e3b63a51d7476985f4f7b1994e
mojidev-py <pycharmdudeig@gmail.com>
2024-09-21 16:57:03 -0700

    self.roles in permission overwrites deleted because it needs to send multiple api reqs, gonna make sep. func

--- discmoji/overwrites.py
+++ discmoji/overwrites.py
@@ -2,15 +2,11 @@ from typing import *
 from .bot import Bot
 from .guild import Guild
 import asyncio
+from .roles import Role
 
 class ChannelPermissionOverwrite:
     
     def __init__(self,_data: dict,__bindedbot: Bot,__bindedguild: Guild):
-        try:
-            self.role = asyncio.run(__bindedbot._http.send_request('get',f'/guilds/{__bindedguild.id}/roles/{_data["permission_overwrites"]["id"] if _data["permission_overwrites"] else None}'))
-        except Exception:
-            # placeholder, since no role obj yet
-            self.roles = [... for asyncio.run(__bindedbot._http.send_request('get',f'/guilds/{__bindedguild.id}/roles/{_data["permission_overwrites"]["id"]}')) in _data["permission_overwrites"]]
+        self.role = asyncio.run(__bindedbot._http.send_request('get',f'/guilds/{__bindedguild.id}/roles/{_data["permission_overwrites"]["id"] if _data["permission_overwrites"] else None}'))
         self.allowed_perms = 0
         self.disabled_perms = 0 
-        
