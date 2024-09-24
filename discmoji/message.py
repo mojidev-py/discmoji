d73803a644d76f5f923870bbdf37a8d9cf4cc9f4
mojidev-py <pycharmdudeig@gmail.com>
2024-09-23 21:23:24 -0700

    started making message obj

--- discmoji/message.py
+++ discmoji/message.py
@@ -1,5 +1,11 @@
 from typing import *
+from .channel import GuildTextChannel
+from .bot import Bot
+import asyncio
 
 
 class Message:
-    pass
+    def __init__(self,_data: dict, bot: Bot):
+        self.id: int = _data["id"]
+        self.channel: GuildTextChannel = GuildTextChannel(asyncio.run(bot._http.send_request('get',f'/channels/{_data["channel_id"]}')).data)
+        
