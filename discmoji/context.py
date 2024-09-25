9bce806a8b5a8f0fe44facd8e495106185cf37a1
mojidev-py <pycharmdudeig@gmail.com>
2024-09-25 13:57:06 -0700

    message error

--- discmoji/context.py
+++ discmoji/context.py
@@ -12,15 +12,16 @@ from .message import Message
 class Invoked:
     """A class that hosts the data of where a prefix/slash command was used."""
     # A class that hosts the data of where a command was used
-    def __init__(self,endpoint: EndpointManager,gateway: GatewayManager,bot: Bot,message: Message | None):
+    def __init__(self,endpoint: EndpointManager,gateway: GatewayManager,bot: Bot,msgid: int):
         self._endpoint = endpoint
         self._gateway = gateway
         self._bot = bot
         # rest will be constructed by internal funcs
         self.member = ...
-        self.message: Message = Message(asyncio.run(self._bot._http.send_request('get',f'/channels/{self.channel.id}/messages/{self.message.id}')))
+        self.message = ...
         self.channel = ...
         self.guild = ...
+        self.__msgid = msgid
     
 
     def _construct(self):
@@ -28,3 +29,5 @@ class Invoked:
         self.member: GuildMember = GuildMember(self._gateway.current_payload.data["member"])
         self.guild: Guild = Guild(asyncio.run(self._endpoint.send_request(method="get",route=f"/guilds/{self._gateway.current_payload.data["guild_id"]}")).data)
         self.channel: GuildTextChannel = GuildTextChannel(asyncio.run(self._endpoint.send_request('get',f'/channels/{self._gateway.current_payload.data["channel_id"]}')).data)
+        self.message: Message = Message(asyncio.run(self._bot._http.send_request('get',f'/channels/{self.channel.id}/messages/{self.__msgid}')).data)
+        
