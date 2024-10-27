1fcdf0b3df0c397869634433758c03fbf3d5206c
mojidev-py <pycharmdudeig@gmail.com>
2024-10-27 10:36:37 -0700

    removed old files, old preserved those

--- discmoji/cli/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-# discmoji.cli
-Contains the source code for the CLI, and some extra utilities, such as checks and logging."""
-
-from ...discmoji import *
-from ._cli import __CLI__
--- discmoji/cli/utils.py
+++
@@ -1,7 +0,0 @@
-"""
-    # discmoji.cli.utils
-    contains useful utilities that can be used for checks. Most checks that are not integrated into the 
-    structures themselves are here.
-"""
-
-...
